189228fff8c63550a19c12a5a96094e0ee262ad9
Maya_Hussein <mayamakram@aucegypt.edu>
2021-02-02 14:43:29 +0300

    This branch contains the declarations and implementations of the knapsack code

---
+++ function1.cpp
@@ -0,0 +1,23 @@
+
+// A utility function that returns maximum  of two integers
+int max(int a, int b) { return (a > b) ? a : b; }
+
+int knapsack(int W, int wt[], int val[], int n)
+{
+	if (n == 0 || W == 0)
+		return 0;
+
+	// IF weight  of the nth term is more than Knapsack capacity W	
+	//then this item cannot be included in the optimal solution
+
+	if(wt[n-1] > W)
+		return knapsack(W, wt, val, n-1)
+	//Return the maximum of two classes:
+	//(1) nth term item included
+	//(2) not included
+	else
+		return max(val[ n -1 ] + knapsack(W - wt[n-1], wt, val, n-1),
+		 	knapsack(W, wt, val, n-1));
+}
+
+
---
+++ function1.h
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+
+ int max(int a, int b);
+ int knapsack(int W, int wt, int val[], int n);
